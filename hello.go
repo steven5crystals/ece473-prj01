1cf73b46e1a4500cb00149cb3c663794348d6ffa
Hacker  ; ) <43499569+steven5crystals@users.noreply.github.com>
2023-09-17 17:15:19 +0000

    Update hello.go

--- hello.go
+++ hello.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	cwidSuffix = "1111" // Replace with your last four CWID digits
+	timeFormat = "2006-01-02 15:04:05"
+)
 
 func main() {
-	fmt.Printf("Hello world!\n")
+	currentTime := time.Now().Format(timeFormat)
+	fmt.Println("Hello, world!")
+	fmt.Printf("It's %s now, and my CWID ending in %s.\n", currentTime, cwidSuffix)
 }
